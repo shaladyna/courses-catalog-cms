5d192b080dfb7b21342a754273e947b8d0634432
Szymon <shaladyna@student.agh.edu.pl>
2026-05-31 19:31:41 +0200

    Dodaj pliki projektów.

---
+++ courses-catalog-cms/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace courses_catalog_cms.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
